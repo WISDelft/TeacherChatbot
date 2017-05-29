23d6947aa527b3b6e5c8b42cfc0d214d479ddc0d
Felipe Moraes <fe.moraesg@gmail.com>
2017-05-29 15:38:31 +0200

    Organizing repository for coding

---
+++ train.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Delft University of Technology - Web Information Systems group
+# http://www.wis.ewi.tudelft.nl/
+# Contributors: Felipe Moraes f.moraes@tudelft.nl
+# Date : 2017-05-29 15:36:22
