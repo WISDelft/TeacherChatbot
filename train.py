9a9eeec0b1a430d38b863a610b29266283a3d18f
Felipe Moraes <fe.moraesg@gmail.com>
2017-05-30 16:56:22 +0200

    Initial version

--- train.py
+++ train.py
@@ -3,3 +3,327 @@
 # http://www.wis.ewi.tudelft.nl/
 # Contributors: Felipe Moraes f.moraes@tudelft.nl
 # Date : 2017-05-29 15:36:22
+
+
+from __future__ import unicode_literals, print_function, division
+from io import open
+import unicodedata
+import string
+import re
+import random
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch import optim
+import torch.nn.functional as F
+from data_loader import prepareData
+from models.models import EncoderRNN, DecoderRNN, AttnDecoderRNN
+
+
+
+######################################################################
+# Training the Model
+# ------------------
+#
+# To train we run the input sentence through the encoder, and keep track
+# of every output and the latest hidden state. Then the decoder is given
+# the ``<SOS>`` token as its first input, and the last hidden state of the
+# decoder as its first hidden state.
+#
+# "Teacher forcing" is the concept of using the real target outputs as
+# each next input, instead of using the decoder's guess as the next input.
+# Using teacher forcing causes it to converge faster but `when the trained
+# network is exploited, it may exhibit
+# instability <http://minds.jacobs-university.de/sites/default/files/uploads/papers/ESNTutorialRev.pdf>`__.
+#
+# You can observe outputs of teacher-forced networks that read with
+# coherent grammar but wander far from the correct translation -
+# intuitively it has learned to represent the output grammar and can "pick
+# up" the meaning once the teacher tells it the first few words, but it
+# has not properly learned how to create the sentence from the translation
+# in the first place.
+#
+# Because of the freedom PyTorch's autograd gives us, we can randomly
+# choose to use teacher forcing or not with a simple if statement. Turn
+# ``teacher_forcing_ratio`` up to use more of it.
+#
+
+
+SOS_token = 0
+EOS_token = 1
+
+
+MAX_LENGTH = 10
+
+eng_prefixes = (
+    "i am ", "i m ",
+    "he is", "he s ",
+    "she is", "she s",
+    "you are", "you re ",
+    "we are", "we re ",
+    "they are", "they re "
+)
+
+
+def indexesFromSentence(lang, sentence):
+    return [lang.word2index[word] for word in sentence.split(' ')]
+
+
+def variableFromSentence(lang, sentence):
+    indexes = indexesFromSentence(lang, sentence)
+    indexes.append(EOS_token)
+    result = Variable(torch.LongTensor(indexes).view(-1, 1))
+    #if use_cuda:
+    #    return result.cuda()
+    #else:
+    return result
+
+
+def variablesFromPair(pair):
+    input_variable = variableFromSentence(input_lang, pair[0])
+    target_variable = variableFromSentence(output_lang, pair[1])
+    return (input_variable, target_variable)
+
+teacher_forcing_ratio = 0.5
+
+
+def train(input_variable, target_variable, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length=MAX_LENGTH):
+    encoder_hidden = encoder.initHidden()
+
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+
+    input_length = input_variable.size()[0]
+    target_length = target_variable.size()[0]
+    
+    encoder_outputs = Variable(torch.zeros(max_length, encoder.hidden_size))
+    encoder_outputs = encoder_outputs.cuda() if use_cuda else encoder_outputs
+   
+    loss = 0
+
+    for ei in range(input_length):
+        encoder_output, encoder_hidden = encoder(
+            input_variable[ei], encoder_hidden)
+        encoder_outputs[ei] = encoder_output[0][0]
+
+    decoder_input = Variable(torch.LongTensor([[SOS_token]]))
+    decoder_input = decoder_input.cuda() if use_cuda else decoder_input
+    
+    decoder_hidden = encoder_hidden
+
+    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+
+    if use_teacher_forcing:
+        # Teacher forcing: Feed the target as the next input
+        for di in range(target_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_output, encoder_outputs)
+            loss += criterion(decoder_output[0], target_variable[di])
+            decoder_input = target_variable[di]  # Teacher forcing
+
+    else:
+        # Without teacher forcing: use its own predictions as the next input
+        for di in range(target_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_output, encoder_outputs)
+            topv, topi = decoder_output.data.topk(1)
+            ni = topi[0][0]
+            
+            decoder_input = Variable(torch.LongTensor([[ni]]))
+            decoder_input = decoder_input.cuda() if use_cuda else decoder_input
+            
+            loss += criterion(decoder_output[0], target_variable[di])
+            if ni == EOS_token:
+                break
+
+    loss.backward()
+
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+
+    return loss.data[0] / target_length
+
+
+######################################################################
+# This is a helper function to print time elapsed and estimated time
+# remaining given the current time and progress %.
+#
+
+import time
+import math
+
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
+
+
+######################################################################
+# The whole training process looks like this:
+#
+# -  Start a timer
+# -  Initialize optimizers and criterion
+# -  Create set of training pairs
+# -  Start empty losses array for plotting
+#
+# Then we call ``train`` many times and occasionally print the progress (%
+# of examples, time so far, estimated time) and average loss.
+#
+
+def save_checkpoint(state, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'model_best.pth.tar')
+
+def trainIters(encoder, decoder, n_iters, print_every=1000, plot_every=100, learning_rate=0.01):
+    start = time.time()
+    plot_losses = []
+    print_loss_total = 0  # Reset every print_every
+    plot_loss_total = 0  # Reset every plot_every
+
+    encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
+    decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
+    training_pairs = [variablesFromPair(random.choice(pairs))
+                      for i in range(n_iters)]
+    
+    criterion = nn.NLLLoss()
+
+    for iter in range(1, n_iters + 1):
+        training_pair = training_pairs[iter - 1]
+        input_variable = training_pair[0]
+        target_variable = training_pair[1]
+ 
+        loss = train(input_variable, target_variable, encoder,
+                     decoder, encoder_optimizer, decoder_optimizer, criterion)
+        print_loss_total += loss
+        plot_loss_total += loss
+
+        if iter % print_every == 0:
+            print_loss_avg = print_loss_total / print_every
+            print_loss_total = 0
+            print('%s (%d %d%%) %.4f' % (timeSince(start, iter / n_iters),
+                                         iter, iter / n_iters * 100, print_loss_avg))
+        
+            save_checkpoint({
+                'epoch': epoch + 1,
+                'arch': args.arch,
+                'state_dict': model.state_dict(),
+                'best_error': best_prec1,
+                'optimizer' : optimizer.state_dict(),
+            }, is_best)
+
+    showPlot(plot_losses)
+
+
+######################################################################
+# Plotting results
+# ----------------
+#
+# Plotting is done with matplotlib, using the array of loss values
+# ``plot_losses`` saved while training.
+#
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+
+
+def showPlot(points):
+    plt.figure()
+    fig, ax = plt.subplots()
+    # this locator puts ticks at regular intervals
+    loc = ticker.MultipleLocator(base=0.2)
+    ax.yaxis.set_major_locator(loc)
+    plt.plot(points)
+
+
+######################################################################
+# Evaluation
+# ==========
+#
+# Evaluation is mostly the same as training, but there are no targets so
+# we simply feed the decoder's predictions back to itself for each step.
+# Every time it predicts a word we add it to the output string, and if it
+# predicts the EOS token we stop there. We also store the decoder's
+# attention outputs for display later.
+#
+
+def evaluate(encoder, decoder, sentence, max_length=MAX_LENGTH):
+    input_variable = variableFromSentence(input_lang, sentence)
+    input_length = input_variable.size()[0]
+    encoder_hidden = encoder.initHidden()
+
+    encoder_outputs = Variable(torch.zeros(max_length, encoder.hidden_size))
+    encoder_outputs = encoder_outputs.cuda() if use_cuda else encoder_outputs
+
+    for ei in range(input_length):
+        encoder_output, encoder_hidden = encoder(input_variable[ei],
+                                                 encoder_hidden)
+        encoder_outputs[ei] = encoder_outputs[ei] + encoder_output[0][0]
+
+    decoder_input = Variable(torch.LongTensor([[SOS_token]]))  # SOS
+    decoder_input = decoder_input.cuda() if use_cuda else decoder_input
+
+    decoder_hidden = encoder_hidden
+
+    decoded_words = []
+    decoder_attentions = torch.zeros(max_length, max_length)
+
+    for di in range(max_length):
+        decoder_output, decoder_hidden, decoder_attention = decoder(
+            decoder_input, decoder_hidden, encoder_output, encoder_outputs)
+        decoder_attentions[di] = decoder_attention.data
+        topv, topi = decoder_output.data.topk(1)
+        ni = topi[0][0]
+        if ni == EOS_token:
+            decoded_words.append('<EOS>')
+            break
+        else:
+            decoded_words.append(output_lang.index2word[ni])
+        
+        decoder_input = Variable(torch.LongTensor([[ni]]))
+        decoder_input = decoder_input.cuda() if use_cuda else decoder_input
+
+    return decoded_words, decoder_attentions[:di + 1]
+
+
+######################################################################
+# We can evaluate random sentences from the training set and print out the
+# input, target, and output to make some subjective quality judgements:
+#
+
+def evaluateRandomly(encoder, decoder, n=10):
+    for i in range(n):
+        pair = random.choice(pairs)
+        print('>', pair[0])
+        print('=', pair[1])
+        output_words, attentions = evaluate(encoder, decoder, pair[0])
+        output_sentence = ' '.join(output_words)
+        print('<', output_sentence)
+        print('')
+
+
+
+input_lang, output_lang, pairs = prepareData('q','a', True)
+print(random.choice(pairs))
+
+use_cuda = False
+hidden_size = 256
+encoder1 = EncoderRNN(input_lang.n_words, hidden_size)
+attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words,
+                               1, dropout_p=0.1)
+
+trainIters(encoder1, attn_decoder1, 75000, print_every=10)
+
+evaluateRandomly(encoder1, attn_decoder1)
+
